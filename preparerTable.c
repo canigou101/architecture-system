af6d36c78e125cbd275d234029db54844bbbe5de
KElscript <elias222@orange.fr>
2025-10-25 23:54:34 +0200

    commentaires

--- preparerTable.c
+++ preparerTable.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 
 int main(int argc, char** argv){
+    /*vérification de la bonne utilisation des paramètres*/
     if (argc!=2) {
         fprintf(stderr,"usage : \n\t%s REPERTOIRE\n", argv[0] );
         exit(EXIT_FAILURE);
@@ -15,23 +16,28 @@ int main(int argc, char** argv){
     char* mes_params[]={"test","-f",nomFichier,(char*)NULL};
     pid_t mon_pid = fork();
     if(mon_pid==0){
+        /*Vérification de l'existence du fichier meta dans la table indiquée*/
         execv("/usr/bin/test",mes_params);
     }else{
         int mon_Code;
         wait(&mon_Code);
         if (mon_Code==0){
+            /*Si le fichier meta existe déjà, arrêt du programme*/
             printf("La table est déjà préparée, si vous voulez la changer, veuillez lancer une réinitialisation. \n");
-            return EXIT_SUCCESS;
+            return EXIT_FAILURE;
         }
         else{
+            /*Si le fichier meta n'existe pas, il est créé*/
             pid_t mon_pid2 = fork();
             if(mon_pid2<0){
                 perror("Le processus ne s'est pas lancé. \n");
                 return(EXIT_FAILURE);
             }else if(mon_pid2==0){
+                /*Création du fichier meta*/
                 char* mesParam2[]= {"touch", nomFichier};
                 execv("/usr/bin/touch",mesParam2);
             }else{
+                /*Si le fichier est créé avec succès, le programme demande à l'utilisateur d'entrer les données voulues*/
                 int mon_Code2;
                 wait(&mon_Code2);
                 if (mon_Code2==0){
@@ -49,6 +55,7 @@ int main(int argc, char** argv){
                     }
                     fclose(fich);
                 }else{
+                    /*En cas d'échec de la commande touch*/
                     printf("echec de la préparation de la table\n");
                     return mon_Code2;
                 }
--- reinitialiserTable.c
+++ reinitialiserTable.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 
 int main(int argc, char** argv){
+    /*vérification de la bonne utilisation des paramètres*/
     if (argc!=2) {
         fprintf(stderr,"usage : \n\t%s REPERTOIRE\n", argv[0] );
         exit(EXIT_FAILURE);
@@ -11,11 +12,13 @@ int main(int argc, char** argv){
     char* mes_params[]={"supprimerTable",argv[1],(char*)NULL};
     pid_t mon_pid = fork();
     if(mon_pid==0){
+        /*suppression de la table dans le processus fils*/
         execv("supprimerTable",mes_params);
     }else{
         int mon_Code;
         wait(&mon_Code);
         if (mon_Code==0){
+            /*si la supression s'est effectuée, on lance la création d'une table vide de même nom dans le processus père*/
             char* mes_params2[]={"creerTable",argv[1],(char*)NULL};
             pid_t mon_pid2 = fork();
             if(mon_pid2==0){
@@ -23,7 +26,7 @@ int main(int argc, char** argv){
             }else{
                 int mon_Code2;
                 wait(&mon_Code2);
-                if (mon_Code2==0){
+                if (mon_Code2==0){ /*vérification de la création de la table*/
                     printf("La table a été réinitialisée avec succès. \n");
                     return EXIT_SUCCESS;
                 }else{
@@ -31,7 +34,7 @@ int main(int argc, char** argv){
                     return mon_Code2;
                 }
             }
-        }else{
+        }else{ /*Si la table n'a pas été supprimée, un message d'erreur est transmis*/
             printf("La table n'existe pas. \n");
             return mon_Code;  
         }
